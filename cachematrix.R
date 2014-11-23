3b8bf80778705c3d681ab81b0b2e02aa41b684e9
Yongdan Tang <tangyongdan@gmail.com>
2014-11-23 20:03:20 +0800

    # supplement additional comments to functions
    Besides the comments added during coding, adding additional explanations to functions using #

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) { #input x will be an matrix
+makeCacheMatrix <- function(x = matrix()) { # This function This function creates a matrix object that can cache its inverse. Input x will be an matrix
   
-     i <- NULL # i will be the inverse and it's reset to NULL 
-    set <- function(y) { 
-      x <<- y          #saves the input vector
-      i <<- NULL       #resets the inverse to NULL
+     i <- NULL # i will be the inverse and it's reset to NULL every time makeCacheMatrix is called
+    
+    set <- function(y) { # this function is used to reset the matrix
+      x <<- y          # x saves the input vector using superassignment
+      i <<- NULL       # resets the inverse to NULL
     }
     
-    get <- function() { x }  #return the value of the original matrix
+    get <- function() { x }  # This funciton returns the value of the original matrix
     
-    setinverse <- function(inv) { i <<- inv} #called by cacheSolve during the first catchSolve 
-                                             #access and it will store the value using superassignment
+    setinverse <- function(inv) { i <<- inv} # This is called by cacheSolve() during the first catchSolve 
+                                             # access and it will store the matrix inverse using superassignment
     
-    getinverse <- function() { i } # return the catchSolve     
+    getinverse <- function() { i } # This function returns the cached matrix inverse on subsequent accesses     
     
-    list(set = set, get = get,
+    list(set = set, get = get,                # This is a list of internal funcitons so a calling function knows how to access these functions
          setinverse = setinverse,
          getinverse = getinverse)
   
@@ -29,19 +29,20 @@ makeCacheMatrix <- function(x = matrix()) { #input x will be an matrix
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) { #input x is an matrix created by makeCacheMatrix
+cacheSolve <- function(x, ...) { # This function computes the inverse of the matrix if the inverse is not available 
+                                 # input x is an matrix created by makeCacheMatrix
         
   ## Return a matrix that is the inverse of 'x'
-  i <- x$getinverse() #access the matrix inverse
+  i <- x$getinverse() # access the matrix inverse
   
-  if(!is.null(i)) {
+  if(!is.null(i)) {              # if the inverse is available in cache, return the cached value               
     message("getting cached data")
     return(i)
   }
   
-  data <- x$get() #if x$getinverse returns NULL, read the data
-  i <- solve(data, ...) #if i was NULL then inverse data
-  x$setinverse(i) #store inversed matrix in i
-  i #return the inversed matrix
+  data <- x$get() # if x$getinverse returns NULL, read the data
+  i <- solve(data, ...) # if i was NULL then inverse the matrix using solve()
+  x$setinverse(i) # store inversed matrix in i
+  i               # return the inversed matrix
   
 }
